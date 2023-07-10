2f620d5909a2edab46b7c64de1bdcdc4558eb37e
vchaindz <dennis@codenotary.com>
2023-07-10 04:28:21 -0400

    add immudb Vault

--- README.md
+++ README.md
@@ -18,19 +18,40 @@ Make sure you have installed the necessary Python packages listed in the `requir
 pip install -r requirements.txt
 ```
 
+You will need the Python Kubernetes client and DeepDiff modules installed.
+
 ### Running the Script
 You can run the script from the command line as follows:
 
 ```
-python k8s_config_monitor.py --kubeconfig /path/to/your/kubeconfig --namespace yournamespace
+python k8s_config_tracker.py --kubeconfig=/path/to/kubeconfig --namespace=monitoring --apikey=your_vault_api_key
 ```
+
+The arguments are as follows:
+
+--kubeconfig: Path to the kubeconfig file. If not provided, the KUBECONFIG environment variable or a default path is used.
+--namespace: The specific namespace to monitor. If not provided, all namespaces are monitored.
+--apikey: The API key for the vault.immudb.io service.
+All these arguments are optional. If not provided, the script will try to use environment variables or a default configuration.
 
 ### Environment Variables
 
 - `KUBECONFIG`: Path to your kubeconfig file. If not set, the script will look for the kubeconfig file at `/path/to/your/kubeconfig`.
 - `NAMESPACE`: Specific namespace to monitor. If not set, the script will monitor all namespaces.
+- `VAULT_IMMUDB_API_KEY`: Specify immudb Vault API Key. Register at [immudb Vault](https://vault.immudb.io)  
 
-## Note
+Operation
+The script first loads the Kubernetes configuration from the specified kubeconfig file, or the KUBECONFIG environment variable or a default path.
 
-This script is primarily for educational and testing purposes. It does not handle potential exceptions that may occur during file operations, such as permissions issues or insufficient disk space. Please enhance error handling if you wish to use it in a production environment.
+It then enters a loop where it retrieves all pods either from the specified namespace, or all namespaces if none was specified. It then checks each pod's configuration against the stored hash of its configuration.
+
+If a pod is new, or its configuration has changed, the new configuration is sent to the vault.immudb.io service, and the configuration is stored locally for future comparisons.
+
+When a configuration change is detected, a difference report is generated detailing which fields changed, and how. This report is sent to the vault and printed to the console.
+
+You can set the KUBECONFIG, NAMESPACE, and VAULT_IMMUDB_API_KEY environment variables instead of providing command line arguments.
+
+## Notes
+
+This script requires Python 3.6+ to run. It uses the Kubernetes and requests Python modules, which can be installed via pip.
 
--- k8svaultaudit.py
+++ k8svaultaudit.py
@@ -2,13 +2,17 @@ import os
 import json
 import hashlib
 import argparse
+import requests
 from kubernetes import client, config, watch
 from datetime import datetime
+from deepdiff import DeepDiff
+from deepdiff.serialization import json_dumps
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Monitor Kubernetes configurations.')
 parser.add_argument('--kubeconfig', type=str, help='Path to the kubeconfig file.')
 parser.add_argument('--namespace', type=str, help='Specific namespace to monitor.')
+parser.add_argument('--apikey', type=str, help='API key for the vault.immudb.io service.')
 args = parser.parse_args()
 
 # Load Kubernetes configuration file from command line argument or environment variable
@@ -17,6 +21,11 @@ config.load_kube_config(kube_config_path)
 
 # Set the namespace to watch
 namespace_to_watch = args.namespace or os.getenv('NAMESPACE', None)
+
+# Get API key for the vault.immudb.io service from command line argument or environment variable
+api_key = args.apikey or os.getenv('VAULT_IMMUDB_API_KEY', None)
+if not api_key:
+    raise Exception('No API key provided for the vault.immudb.io service.')
 
 v1 = client.CoreV1Api()
 
@@ -28,35 +37,97 @@ def datetime_handler(x):
         return x.isoformat()
     raise TypeError("Unknown type")
 
+# Creating a Session to handle the requests
+s = requests.Session()
+s.headers.update({"Content-Type": "application/json", "X-API-KEY": api_key})
+
 while True:
     # Get all pods
     if namespace_to_watch:
         pods = v1.list_namespaced_pod(namespace=namespace_to_watch, watch=False)
     else:
         pods = v1.list_pod_for_all_namespaces(watch=False)
-    
+
     for pod in pods.items:
         # Convert pod data to JSON, handling datetime objects
-        pod_data = json.dumps(pod.to_dict(), sort_keys=True, default=datetime_handler)
-        # Hash the JSON data
-        pod_hash = hashlib.sha256(pod_data.encode('utf-8')).hexdigest()
+        pod_data = json.loads(json.dumps(pod.to_dict(), sort_keys=True, default=datetime_handler))
+        pod_data["itemtype"] = "kubernetesconfig"
         
-        if pod.metadata.name not in pod_data_store:
-            # If the pod is new, add it to the data store
-            pod_data_store[pod.metadata.name] = {'hash': pod_hash, 'config': pod_data}
-        elif pod_data_store[pod.metadata.name]['hash'] != pod_hash:
-            # If the pod configuration has changed, update the data store and print a message
-            old_config_file_path = f'{pod.metadata.name}_old_config.json'
-            old_config_file = open(old_config_file_path, 'w')
-            old_config_file.write(pod_data_store[pod.metadata.name]['config'])
-            old_config_file.close()
-            print(f"Previous configuration for pod {pod.metadata.name} has been written to {old_config_file_path}")
+        # Hash the JSON data
+        pod_hash = hashlib.sha256(json.dumps(pod_data, sort_keys=True).encode('utf-8')).hexdigest()
 
+        if pod.metadata.name not in pod_data_store:
+            # If the pod is new, add it to the data store and send to immudb.io
             pod_data_store[pod.metadata.name] = {'hash': pod_hash, 'config': pod_data}
+            inserted = s.put("https://vault.immudb.io/ics/api/v1/ledger/default/collection/default/document", 
+                             json=pod_data)
+            assert inserted.status_code == 200
+            print(f"New configuration for pod {pod.metadata.name} has been sent to the vault")
+
+        elif pod_data_store[pod.metadata.name]['hash'] != pod_hash:
+            # If the pod configuration has changed, update the data store and send new config to immudb.io
+            old_config = pod_data_store[pod.metadata.name]['config']
+            new_config = pod_data
             
-            new_config_file_path = f'{pod.metadata.name}_new_config.json'
-            new_config_file = open(new_config_file_path, 'w')
-            new_config_file.write(pod_data)
-            new_config_file.close()
-            print(f"New configuration for pod {pod.metadata.name} has been written to {new_config_file_path}")
+            # Send new configuration to immudb.io
+            inserted = s.put("https://vault.immudb.io/ics/api/v1/ledger/default/collection/default/document", 
+                             json=new_config)
+            assert inserted.status_code == 200
+            print(f"Updated configuration for pod {pod.metadata.name} has been sent to the vault")
+            
+            # Update the pod data store
+            pod_data_store[pod.metadata.name] = {'hash': pod_hash, 'config': new_config}
+
+            # Get the last two entries for this pod from immudb.io
+            lastTwoEntries = s.post("https://vault.immudb.io/ics/api/v1/ledger/default/collection/default/documents/search", 
+                                    json={
+                                        "page": 0,
+                                        "perPage": 2,
+                                        "orderBy": [{"desc": True, "field": "_id"}],
+                                        "query": {
+                                            "expression": {
+                                                "fieldComparisons": {
+                                                    "field": "itemtype",
+                                                    "operator": "EQ",
+                                                    "value": "kubernetesconfig"
+                                                }
+                                            }
+                                        }
+                                    })
+
+            # If there are no previous revisions, continue with the next pod
+            if 'revisions' not in lastTwoEntries.json():
+                continue
+            
+            firstDocument = lastTwoEntries.json()["revisions"][0]["document"]
+            secondDocument = lastTwoEntries.json()["revisions"][1]["document"]
+
+            # Use DeepDiff to find the changes between the old and new configuration
+            differencer = DeepDiff(firstDocument, secondDocument, ignore_order=True, exclude_paths=["root['_id']", "root['_vault_md']"])
+            dicted = differencer.to_dict()
+            dicted["itemtype"] = "kubernetesconfigchange"
+            jsoned = json_dumps(dicted, default_mapping=None)
+            
+            # Send the changes to immudb.io
+            inserted = s.put("https://vault.immudb.io/ics/api/v1/ledger/default/collection/default/document", 
+                             data=jsoned)
+            assert inserted.status_code == 200
+
+            # Print the fields that have changed
+            changes = differencer.to_dict()
+            for change_type, details in changes.items():
+                if change_type == 'values_changed':
+                    print("The following fields have changed values:")
+                    for path, changes in details.items():
+                        print(f"Field: {path}, old value: {changes['old_value']}, new value: {changes['new_value']}")
+                elif change_type in ('iterable_item_added', 'iterable_item_removed'):
+                    print(f"Items have been {change_type} in the following fields:")
+                    for path in details:
+                        print(f"Field: {path}")
+                elif change_type == 'type_changes':
+                    print("The following fields have changed type:")
+                    for path, changes in details.items():
+                        print(f"Field: {path}, old type: {changes['old_type']}, new type: {changes['new_type']}")
+
+            print(f"Configuration change for pod {pod.metadata.name} has been sent to the vault")
 
